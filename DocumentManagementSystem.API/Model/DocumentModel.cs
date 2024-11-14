b9becd2d56eefcdf8f714a2bd3dc177dc95dc9a5
Nahi Islam Jashim <if23b059@technikum-wien.at>
2024-11-14 15:12:09 +0100

    moved our model class to a separate project

--- DocumentManagementSystem.API/Model/DocumentModel.cs
+++ DocumentManagementSystem.API/Model/DocumentModel.cs
@@ -1,10 +1,10 @@
-namespace DocumentManagementSystem.API.Model
+namespace Model
 {
     public class DocumentModel
     {
         public Guid Id { get; set; } = Guid.NewGuid();
 
-        public string? Title { get; set; }
+        public string Name { get; set; }
 
         //public string Content { get; set; }
 
