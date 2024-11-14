3b3982dc0a577bf830642a094c401c386cc7f6fa
Nahi Islam Jashim <if23b059@technikum-wien.at>
2024-11-14 15:18:55 +0100

    added mappingprofile

---
+++ DocumentManagementSystem.API/BusinessLayer/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DataAccessLayer.Entity;
+using Model;
+
+namespace BusinessLayer.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Document, DocumentModel>().ReverseMap();
+        }
+    }
+}
