ceb8c7d346e55275eded7e50f436bcab03414a57
Nahi Islam Jashim <if23b059@technikum-wien.at>
2024-11-24 04:26:29 +0100

    added businesslayer

---
+++ DocumentManagementSystem/BusinessLayer/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Model;
+using System.Reflection.Metadata;
+
+namespace BusinessLayer.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Document, DocumentModel>().ReverseMap();
+        }
+    }
+}
---
+++ DocumentManagementSystem/BusinessLayer/Service/Interface/IDocumentService.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+
+namespace BusinessLayer.Service.Interface
+{
+    public interface IDocumentService
+    {
+        Task AddDocumentAsync(DocumentModel newDocumentModel);
+
+        Task<List<DocumentModel>> GetDocumentsAsync();
+
+        Task<DocumentModel> GetDocumentByIdAsync(Guid documentModelId);
+
+        Task UpdateDocumentAsync(DocumentModel updatedDocumentModel);
+
+        Task DeleteDocumentAsync(Guid documentModelId);
+    }
+}
